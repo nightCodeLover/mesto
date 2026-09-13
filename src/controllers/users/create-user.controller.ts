9d4a5180a9aa4808de3285b29e96e235540bc1eb
Vladimir Maltsev <mva.nbd@gmail.com>
2026-09-13 15:51:43 +0300

    refactor

--- src/controllers/users/create-user.controller.ts
+++ src/controllers/users/create-user.controller.ts
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import { CREATED_STATUS_CODE } from "../../constants";
 import { User, UserModel } from "../../models";
-import { BadRequestError, userErrors } from "../../errors";
+import { BadRequestError, ConflictError, userErrors } from "../../errors";
 
 const SALT_LENGTH = 16;
+
+const USER_EXIST_CODE = 11000;
 
 export const createUserController = async (
   req: Request<Record<string, never>, unknown, UserModel>,
   res: Response,
 ) => {
-  const {
-    name, avatar, about, password, email,
-  } = req.body;
+  const { name, avatar, about, password, email } = req.body;
 
   try {
     const hashPas = await bcrypt.hash(password, SALT_LENGTH);
@@ -33,6 +33,13 @@ export const createUserController = async (
       email: createdUser.email,
     });
   } catch (error) {
+    if (
+      error instanceof mongoose.mongo.MongoServerError &&
+      error.code === USER_EXIST_CODE
+    ) {
+      throw new ConflictError(userErrors.userExistMessage);
+    }
+
     if (error instanceof mongoose.Error.ValidationError) {
       throw new BadRequestError(userErrors.incorrectPostDataMessage);
     }
